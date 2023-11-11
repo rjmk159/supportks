4beb9234d3b00e0f0366f19a6546aaca9d80c5dd
Raja Junaid <rjmk159@gmail.com>
2023-11-11 16:16:41 +0530

    Adding Document download to doc button width fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -99,8 +99,8 @@ export default function Home() {
         </h2>
       </div>
       <div className="flexflex-col justify-between">
-        <div className="max-w-4xl mx-auto bg-white p-6 shadow-md rounded-md">
-          <div className="flex justify-center space-between mb-4">
+        <div className="max-w-4xl mx-auto bg-white p-6 shadow-md rounded-md ">
+          <div className="flex justify-center space-between mb-4 flex-w">
             <CopyToClipboard
               className="bg-blue-500 m-2 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800 flex items-center"
               text={content}
