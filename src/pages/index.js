dd9c5e11335ee9eff25446d7eee6af55233d442c
Raja Junaid <rjmk159@gmail.com>
2023-11-11 15:21:36 +0530

    Text changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,26 +10,28 @@ const content = `Subject: Request to Add Kashmiri Language in Perso-Arabic scrip
 
 Dear Google Translation Team,
 
-We are writing to request the inclusion of the Kashmiri language in Nastaliq script into Google's translation services, along with vocal support. Kashmiri, with more than 7.1 million speakers globally, holds immense significance as one of the official languages of the Indian region of Jammu and Kashmir.
+We are writing to request the inclusion of the Kashmiri language in institutionally approved Nastaliq script into Google's translation services, along with vocal support. Kashmiri, with more than 7.1 million speakers globally.
 
-Kashmiri, written in the beautiful Nastaliq script, is deeply rooted in the rich cultural tapestry of the region. The absence of Kashmiri in Google's translation services, including vocal support, poses a significant linguistic and cultural barrier. 
+As per records available on different google platforms, Kashmiri is 5000 years old language and has strong literary traditions of about 8 Centuries. A few languages in the contemporary times enjoy the history of being so old. It is elder to many languages of the Indo Iranian and Indo Aryan origin. This language has a strong vocabulary system and can cater the demands of technological era. Besides Jammu and Kashmir, it is spoken in many parts of subcontinent and different parts of the world by Kashmiri diaspora. with rich literary traditions and strong cultural bonds this language has played a vital role in promoting human values ,peace ,interfaith harmony and high standards of literature writing .
 
-Kashmir is not only a region with a rich linguistic and cultural heritage but is also an officially recognized tourist destination. The inclusion of Kashmiri in Google Translate, written in Nastaliq script, will enhance the tourist experience, foster better communication, and promote economic growth in the region. Tourists visiting Kashmir will have a more immersive experience, locals will be better able to cater to their needs, and this, in turn, will stimulate economic conditions in the area.
+Kashmiri, is deeply rooted in the rich cultural tapestry of the region. The absence of Kashmiri in Google's translation services, including vocal support, poses a significant linguistic and cultural barrier.
+
+Kashmir is not only a region with a rich linguistic and cultural heritage but is also an officially recognized tourist destination. The inclusion of Kashmiri in Google Translate will enhance the tourist experience, foster better communication, and promote economic growth in the region. Tourists visiting Kashmir will have a more immersive experience, locals will be better able to cater to their needs, and this, in turn, will stimulate economic conditions in the area.
 
 We are eager to collaborate with Google in this endeavor and are prepared to provide any necessary linguistic resources, documentation, and expert support to expedite the process of adding Kashmiri in Nastaliq script to Google Translate. Our community is dedicated and enthusiastic about contributing to this effort.
 
-Furthermore, Kashmiri plays a vital role as a currency language in the Indian region of Jammu and Kashmir. Its inclusion in Google Translate, supported by vocal capabilities, will facilitate 
-trade, commerce, and everyday transactions, contributing to the economic empowerment of millions of individuals who rely on this language for their livelihoods.
+Furthermore, Kashmiri plays a vital role as a currency language in Jammu and Kashmir. Its inclusion in Google Translate, supported by vocal capabilities, will facilitate trade, commerce, and everyday transactions, contributing to the economic empowerment of millions of individuals who rely on this language for their livelihoods.
 
-We're excited to help Google add Kashmiri in Nastaliq script to Translate, offering linguistic resources, expert support, and a prescribed grammar. Our dedicated community, with thousands of books, dictionaries, and a running newspaper, is enthusiastic about contributing to this effort
+We're excited to help Google add Kashmiri to Translate, offering linguistic resources, expert support, and a prescribed grammar. Our dedicated community, with thousands of books, dictionaries, and a running newspaper, is enthusiastic about contributing to this effort
 
-In our pursuit of this goal, we aim to garner support from the broader linguistic and language professional community. Here are a few email addresses of NGO’s or individuals who will be able to provide invaluable assistance in this endeavor:
+In our pursuit of this goal, we aim to garner support from the broader linguistic and language professional community. Here are a few email addresses of NGO’s or individuals who will be able to provide invaluable assistance in this endeavor
 
 amkjammukashmir@gmail.com
 rjmk1359@gmail.com
 
 
 We kindly request Google to initiate a dialogue with our community and these experts to discuss the process of adding Kashmiri in Nastaliq script to Google Translate, complete with vocal support. Together, we can make this vision a reality, enhancing accessibility for all.
+
 
 Thank you for your attention and consideration.
 
@@ -74,8 +76,7 @@ export default function Home() {
       <div className="max-w-600">
         <h2 className="text-2xl font-bold mb-4 text-center p-6 ">
           Copy the text below and follow the steps to submit your request to
-          Google to Add Kashmiri Language to
-          Google Translation Services.
+          Google to Add Kashmiri Language to Google Translation Services.
           <br />
           <div
             className="bg-yellow-200 border-l-4 border-yellow-400 text-yellow-800 p-2 mt-2"
@@ -83,8 +84,8 @@ export default function Home() {
           >
             <p>
               نیچے دیے گئے مواد کو کاپی کریں اور گوگل ٹرانسلیشن سروسز میں کشمیری
-              زبان کو فارسی عربی رسم الخط (نستالق) میں شامل کرنے کے لیے گوگل کو
-              اپنی درخواست جمع کرانے کے لیے اقدامات پر عمل کریں۔
+              میں شامل کرنے کے لیے گوگل کو اپنی درخواست جمع کرانے کے لیے اقدامات
+              پر عمل کریں۔
             </p>
           </div>
         </h2>
@@ -106,29 +107,45 @@ export default function Home() {
           </div>
           <p>
             <b>
-              Subject: Request to Add Kashmiri Language in Perso-Arabic script
+              Subject: Request to Add Kashmiri Language its approved script
               (Nastaliq) to Google Translation Services
             </b>
           </p>
           <p>Dear Google Translation Team,</p>
           <p>
-            {`We are writing to request the inclusion of the Kashmiri language in Nastaliq script into Google's translation services,
-        along with vocal support. Kashmiri, with more than 7.1 million speakers globally, holds immense significance as one of
-        the official languages of the Indian region of Jammu and Kashmir.`}
+            We are writing to request the inclusion of the Kashmiri language in
+            institutionally approved Nastaliq script into Google's translation
+            services, along with vocal support. Kashmiri, with more than 7.1
+            million speakers globally.
           </p>
           <p>
-            {`Kashmiri, written in the beautiful Nastaliq script, is deeply rooted in the rich cultural tapestry of the region. The absence
-        of Kashmiri in Google's translation services, including vocal support, poses a significant linguistic and cultural barrier.`}
+            As per records available on different google platforms, Kashmiri is
+            5000 years old language and has strong literary traditions of about
+            8 Centuries. A few languages in the contemporary times enjoy the
+            history of being so old. It is elder to many languages of the Indo
+            Iranian and Indo Aryan origin. This language has a strong vocabulary
+            system and can cater the demands of technological era. Besides Jammu
+            and Kashmir, it is spoken in many parts of subcontinent and
+            different parts of the world by Kashmiri diaspora. with rich
+            literary traditions and strong cultural bonds this language has
+            played a vital role in promoting human values ,peace ,interfaith
+            harmony and high standards of literature writing .
+          </p>
+          <p>
+            Kashmiri, is deeply rooted in the rich cultural tapestry of the
+            region. The absence of Kashmiri in Google's translation services,
+            including vocal support, poses a significant linguistic and cultural
+            barrier.{" "}
           </p>
           <p>
             Kashmir is not only a region with a rich linguistic and cultural
             heritage but is also an officially recognized tourist destination.
-            The inclusion of Kashmiri in Google Translate, written in Nastaliq
-            script, will enhance the tourist experience, foster better
-            communication, and promote economic growth in the region. Tourists
-            visiting Kashmir will have a more immersive experience, locals will
-            be better able to cater to their needs, and this, in turn, will
-            stimulate economic conditions in the area.
+            The inclusion of Kashmiri in Google Translate will enhance the
+            tourist experience, foster better communication, and promote
+            economic growth in the region. Tourists visiting Kashmir will have a
+            more immersive experience, locals will be better able to cater to
+            their needs, and this, in turn, will stimulate economic conditions
+            in the area.
           </p>
           <p>
             We are eager to collaborate with Google in this endeavor and are
@@ -139,24 +156,22 @@ export default function Home() {
           </p>
           <p>
             Furthermore, Kashmiri plays a vital role as a currency language in
-            the Indian region of Jammu and Kashmir. Its inclusion in Google
-            Translate, supported by vocal capabilities, will facilitate trade,
-            commerce, and everyday transactions, contributing to the economic
-            empowerment of millions of individuals who rely on this language for
-            their livelihoods.
+            Jammu and Kashmir. Its inclusion in Google Translate, supported by
+            vocal capabilities, will facilitate trade, commerce, and everyday
+            transactions, contributing to the economic empowerment of millions
+            of individuals who rely on this language for their livelihoods.
           </p>
           <p>
-            We're excited to help Google add Kashmiri in Nastaliq script to
-            Translate, offering linguistic resources, expert support, and a
-            prescribed grammar. Our dedicated community, with thousands of
-            books, dictionaries, and a running newspaper, is enthusiastic about
-            contributing to this effort
+            We're excited to help Google add Kashmiri to Translate, offering
+            linguistic resources, expert support, and a prescribed grammar. Our
+            dedicated community, with thousands of books, dictionaries, and a
+            running newspaper, is enthusiastic about contributing to this effort
           </p>
           <p>
             In our pursuit of this goal, we aim to garner support from the
             broader linguistic and language professional community. Here are a
             few email addresses of NGO’s or individuals who will be able to
-            provide invaluable assistance in this endeavor:
+            provide invaluable assistance in this endeavor
           </p>
           <ul>
             <li>
