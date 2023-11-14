088155a23ae8acc26ab9bc55377eda68d8211c36
Raja Junaid <rjmk159@gmail.com>
2023-11-15 04:52:00 +0530

    new changes related to components3

--- src/omponents/Form.jsx
+++ src/omponents/Form.jsx
@@ -39,7 +39,7 @@ const ThumbsForm = ({ handleSubmitFeedback }) => {
       <div className="flex justify-between">
         <button
           onClick={handleThumbsUp}
-          className="bg-blue-700  text-white font-bold py-2 px-2 rounded hover:bg-blue-800 focus:outline-none focus:shadow-outline-blue active:bg-blue-800 flex items-center"
+          className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         >
           {selected === "up" ? (
             <BsHandThumbsUpFill className="w-7 h-7 mr-2" />
@@ -50,7 +50,7 @@ const ThumbsForm = ({ handleSubmitFeedback }) => {
         </button>
         <button
           onClick={handleThumbsDown}
-          className="bg-blue-700  text-white font-bold py-2 px-2 rounded hover:bg-blue-800 focus:outline-none focus:shadow-outline-blue active:bg-blue-800 flex items-center"
+          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
         >
           {selected === "down" ? (
             <BsHandThumbsDownFill className="w-7 h-7 mr-2" />
