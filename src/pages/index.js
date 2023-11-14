eecc6295b286cd8171527e9d5a80c2a983451389
Raja Junaid <rjmk159@gmail.com>
2023-11-15 04:42:30 +0530

    Fx

--- src/pages/index.js
+++ src/pages/index.js
@@ -344,7 +344,7 @@ export default function Home() {
         <button
           onClick={handleToggleVideo}
           style={{ maxWidth: 300, margin: "auto" }}
-          className="bg-blue-900  text-white font-bold py-2 px-2 rounded hover:bg-blue-800 focus:outline-none focus:shadow-outline-blue active:bg-blue-800 flex items-center"
+          className="bg-blue-700  text-white font-bold py-2 px-2 rounded hover:bg-blue-800 focus:outline-none focus:shadow-outline-blue active:bg-blue-800 flex items-center"
         >
           <BsFillCameraVideoFill className="w-5 h-5 mr-2" />{" "}
           {showVideo ? "Hide Video" : "Watch Video"}
